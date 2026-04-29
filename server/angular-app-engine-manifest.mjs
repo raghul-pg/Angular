0efb38d8a453ce00b92f73afd607919c28f5bb40
Raghul G <raghulpg6@gmail.com>
2026-04-29 21:30:52 +0530

    Auto-generated commit

--- server/angular-app-engine-manifest.mjs
+++ server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: '/your-repo-name',
+  basePath: '/Angular',
   allowedHosts: [],
   supportedLocales: {
   "en-US": ""
--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -2,19 +2,19 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/your-repo-name/',
+  baseHref: '/Angular/',
   locale: undefined,
   routes: [
   {
     "renderMode": 2,
-    "route": "/your-repo-name"
+    "route": "/Angular"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 1037, hash: 'd5f5f18bcc18f8d55d86e8d0568bd13616545ec25d96f9c9bde5aa1b01267b73', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 962, hash: '3d1f82157a0706b29c1d74490bd9b2d5dac10f958150d95a643b23737bb5dee5', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 16459, hash: '3d3a8adb5c014bb906ff8ca54fd853f315fa9bd4a6767e29ccfda98d3d4f7d63', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 1030, hash: 'e9aa263e7aa1a4c8ac2a6a176488f21eb2d759cf02e6aa82e839388d72d83f09', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 955, hash: '4d9471f9b227a29eb8398247891937ed91148f617007978d31b43923fe6d1d89', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 16452, hash: 'c39ed3172fda22bdbc86a065e5b08cad422bdf760aae62d3f7f4f402c502fdf6', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-XEDEUV75.css': {size: 470, hash: 'WKdclMM65EE', text: () => import('./assets-chunks/styles-XEDEUV75_css.mjs').then(m => m.default)}
   },
 };
